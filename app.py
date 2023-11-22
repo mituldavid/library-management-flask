c09472d374d11edaf413cdcad67bd976e1c4d9e5
adi457 <138023777+adi457@users.noreply.github.com>
2023-11-22 20:41:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 # MySQL Configuration
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_PORT'] = 3306
 app.config['MYSQL_DB'] = 'librarydb'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
